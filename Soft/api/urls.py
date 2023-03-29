f6e80ad98b5f05a4d6c9aea55c886f06ae2f33d4
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-03-29 18:18:19 +0500

    ishlamadi
    blaaa

--- Soft/api/urls.py
+++ Soft/api/urls.py
@@ -8,13 +8,15 @@ urlpatterns = [
     path('gallery/', GalerrySerializerAPI.as_view()),
     path('our/', OurProjectsSerializerAPI.as_view()),
     path('hometitle/', HomeTitleSerializerAPI.as_view()),
+    path('ourservis/', OurCoreSevisSerializerAPI.as_view()),
 
     # detail update url
     path('change/<int:pk>/', BaseUpdatedSerializer.as_view()),
     path('about/<int:pk>/', AboutUpdatedSerializer.as_view()),
     path('gallery/<int:id>/', GalleryUpdatedSerializer.as_view()),
     path('our/<int:id>/', OurUpdatedSerializer.as_view()),
-    path('hometitle/<int:id>/', HomeTitleUpdateSerializerAPI.as_view())
+    path('hometitle/<int:id>/', HomeTitleUpdateSerializerAPI.as_view()),
+    path('ourservis/<int:id>/', OurCoreSevisUpdateSerializerAPI.as_view())
 
 ]
 
