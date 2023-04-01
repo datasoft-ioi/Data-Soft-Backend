585049adff32126bad06585ed5cfdbbcac5d6669
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-04-01 17:18:40 +0500

    add: CSRF_TRUSTED_ORIGINS

--- Soft/config/settings.py
+++ Soft/config/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ['https://soft.datashop.uz']
+CSRF_TRUSTED_ORIGINS = ['https://soft.datashop.uz', 'https://api.data-soft.uz']
 
 
 ROOT_URLCONF = 'config.urls'
